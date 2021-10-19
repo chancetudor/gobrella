da7a5a3773dd5de5b185fb9fe4f3593f65e1a01e
cmtudor <31376942+chancetudor@users.noreply.github.com>
2021-10-18 21:44:27 -0400

    implementing GetDestinations function

--- client/destinationsEndpoints.go
+++ client/destinationsEndpoints.go
@@ -6,9 +6,10 @@ import (
 )
 
 // GetDestinations returns a list of destination related to a destination list.
-// The function takes a string and returns a Destination or an error, if there was one.
+// The function takes a destination list ID and two optional, integer parameters
+// and returns a Destination or an error, if there was one.
 func (client *UmbrellaClient) GetDestinations(listID string) (dest.Destination, error) {
-	url, err := formURL(client.BaseURL.String(), "destinationlists", listID, "destinations")
+	url, err := formURL(client.BaseURL.String(), "destinationlists", listID)
 	if err != nil {
 		return dest.Destination{}, err
 	}
@@ -22,39 +23,4 @@ func (client *UmbrellaClient) GetDestinations(listID string) (dest.Destination, 
 	}
 
 	return d, nil
-}
-
-// PostDestinations adds a list of destinations to a destination list.
-// The function takes a string and a slice of type Destination and
-// returns a slice of type byte and an error, if there was one.
-func (client *UmbrellaClient) PostDestinations(listID string, dests []dest.Destination) ([]byte, error) {
-	url, err := formURL(client.BaseURL.String(), "destinationlists", listID, "destinations")
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := json.Marshal(dests)
-	resp, err := client.post(url.String(), body)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
-// DeleteDestinations deletes a list of destinations from a destination list.
-// The function takes a string and a slice of type int and returns
-// a slice of type byte and an error, if there was one.
-// The slice parameter should contain a specific destination ID at each index.
-func (client *UmbrellaClient) DeleteDestinations(listID string, destIDs []int) ([]byte, error) {
-	url, err := formURL(client.BaseURL.String(), "destinationlists", listID, "destinations", "remove")
-	if err != nil {
-		return nil, err
-	}
-	body, err := json.Marshal(destIDs)
-	resp, err := client.post(url.String(), body)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
 }
