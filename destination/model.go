bc15c34158e63debc515736f09701d1aac1df940
cmtudor <31376942+chancetudor@users.noreply.github.com>
2021-10-19 16:33:24 -0400

    wrote all endpoints for destinationsEndpoints.go. need to implement tests.

--- destination/model.go
+++ destination/model.go
@@ -4,16 +4,23 @@ import (
 	"encoding/json"
 )
 
+// Destination represents what is held in DestinationLists.
+// When performing a GET request, all fields in the Destination struct are filled.
+// When performing a POST request, all that is needed to be initialized is Destination and a Comment.
+// E.g., NewDestination("google.com", "sample comment for destination")
 type Destination struct {
-	ID          string `json:"id"`
-	Destination string `json:"destination"`
-	Type        string `json:"type"`
-	Comment     string `json:"comment"`
-	CreatedAt   string `json:"createdAt"`
+	ID          string `json:"id,omitempty"`
+	Destination string `json:"destination,omitempty"`
+	Type        string `json:"type,omitempty"`
+	Comment     string `json:"comment,omitempty"`
+	CreatedAt   string `json:"createdAt,omitempty"`
 }
 
-// unmarshal is a helper method to unmarshal an http.Response body
-// into a Destination struct.
+func NewDestination(destination string, comment string) *Destination {
+	return &Destination{Destination: destination, Comment: comment}
+}
+
+// unmarshal is a helper method to unmarshal an http.Response body into a Destination struct.
 // The function takes a slice of bytes, the response body,
 // and returns an error, if there was one.
 func (d *Destination) unmarshal(response []byte) error {
@@ -24,9 +31,10 @@ func (d *Destination) unmarshal(response []byte) error {
 	return nil
 }
 
-// marshal is a helper method to marshal a Destination struct into a JSON encoding.
-// The function returns a slice of bytes, representing the encoded struct, and an error, if there was one.
-func (d *Destination) marshal() ([]byte, error) {
+// Marshal is a helper method to marshal a Destination struct into a JSON encoding.
+// The function returns a slice of bytes, representing the encoded struct,
+// and an error, if there was one.
+func (d *Destination) Marshal() ([]byte, error) {
 	destJSON, err := json.Marshal(d)
 	if err != nil {
 		return nil, err
