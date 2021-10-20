2f19f849fd8041dc489f05e950fdca7b5228dbc0
cmtudor <31376942+chancetudor@users.noreply.github.com>
2021-10-20 15:02:46 -0400

    added custom errors for destinations and refactored my post and get helper functions to return *http.Responses

--- destination/model.go
+++ destination/model.go
@@ -1,31 +1,58 @@
 package destination
 
-// Destination represents what is held in DestinationLists.
-// When performing a GET request, all fields in the Destination struct are filled.
-// When performing a POST request, all that is needed to be initialized is Destination and a Comment.
-// E.g., NewDestination("google.com", "sample comment for destination")
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// Destination represents what is held in DestinationLists and is returned from GetDestinations().
+// When performing a GET request, all fields in the Destination struct are filled, but what is of interest
+// is within the Data field.
 type Destination struct {
-	ID          string `json:"id,omitempty"`
-	Destination string `json:"destination,omitempty"`
-	Type        string `json:"type,omitempty"`
-	Comment     string `json:"comment,omitempty"`
-	CreatedAt   string `json:"createdAt,omitempty"`
+	Status struct {
+		Code int    `json:"code,omitempty"`
+		Text string `json:"text,omitempty"`
+	} `json:"status,omitempty"`
+	Meta struct {
+		Page  int `json:"page,omitempty"`
+		Limit int `json:"limit,omitempty"`
+		Total int `json:"total,omitempty"`
+	} `json:"meta,omitempty"`
+	Data []struct {
+		ID          string `json:"id,omitempty"`
+		Destination string `json:"destination,omitempty"`
+		Type        string `json:"type,omitempty"`
+		Comment     string `json:"comment,omitempty"`
+		CreatedAt   string `json:"createdAt,omitempty"`
+	} `json:"data,omitempty"`
 }
 
-func NewDestination(destination string, comment string) *Destination {
-	return &Destination{Destination: destination, Comment: comment}
+// PostDestination is a struct required for POSTing new destinations.
+// A destination is required but a comment can be omitted by passing an empty string.
+type PostDestination struct {
+	Destination string `json:"destination"`
+	Comment     string `json:"comment,omitempty"`
+}
+
+func NewPostDestination(destination string, comment string) *PostDestination {
+	return &PostDestination{Destination: destination, Comment: comment}
 }
 
 // unmarshal is a helper method to unmarshal an http.Response body into a Destination struct.
 // The function takes a slice of bytes, the response body,
 // and returns an error, if there was one.
-// func (d *Destination) unmarshal(response []byte) error {
-// 	if err := json.Unmarshal(response, d); err != nil {
-// 		return err
-// 	}
-//
-// 	return nil
-// }
+func (d *Destination) Unmarshal(response *http.Response) error {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, d); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Marshal is a helper method to marshal a Destination struct into a JSON encoding.
 // The function returns a slice of bytes, representing the encoded struct,
